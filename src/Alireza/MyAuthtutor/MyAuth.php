8d036df25d3b9021dd7e9e6f3ec36c6a365b7a9b
Alireza Rahmani Khalili <alirezarahmani@live.com>
2015-03-01 14:43:30 +0330

    Update MyAuth.php

--- src/Alireza/MyAuthtutor/MyAuth.php
+++ src/Alireza/MyAuthtutor/MyAuth.php
@@ -1,57 +1,43 @@
 <?php
 
-namespace Models;
-namespace Alireza\MyAuthtutor;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Redirect;
-use User;
-Class MyAuth
+namespace Alireza\Myauthtutor;
+use Request,Session,Input,Redirect,Hash;
+use App\User;
+Class Myauth
 {
     protected $data;
-    public $redirect_login;
-    public $redirect_logout;
+
+    public $redirect_login='/users/home';
+    public $redirect_logout='/users/logout';
 
     public  function __construct()
-    {
-        $this->redirect_login='/users/home';
-        $this->redirect_logout='/users/logout';
+    {  
         if (Request::isMethod('post'))
-        @   $this->data=array('username'=>Input::get('username'),'password'=>Input::get('password'));
+           $this->data=array('username'=>Input::get('username'),'password'=>Input::get('password'));
     }
-
     public function login($data=false)
     {
         $this->data=$data;
-        if($this->data)
-            if(!is_array($this->data))
-                return Redirect::to($this->redirect_login)->with('message', 'sorry no array to log-in manually');
+
+       if ($this->data && !is_array($this->data))
+                return redirect($this->redirect_login)->with('message', 'sorry no array to log-in manually');
 
         if($this->data && !Session::has('user'))
         {
             $result= User::Where(function($query)
             {
                 $query-> where('email',$this->data['username'])
-                      -> where('password',sha1($this->data['password']));
-            })->get();
-
-            foreach($result as $result)
-            {
+                      -> where('password',Hash::make($this->data['password']));
+            })->first();
                 Session::put('user', $result);
-                return Redirect::to('home')->with('message', 'Welcome log-in  succeeded ');
-            }
+            return Redirect('home')->with('message', 'Welcome log-in succeeded ');
                 Session::flush();
-            return Redirect::to($this->redirect_login)->with('message', 'Login Failed, wrong username or password');
+            return redirect($this->redirect_login)->with('message', 'Login Failed, wrong username or password');
         }
-
     }
-
     public function logout()
     {
         Session::flush();
-        return Redirect::to($this->redirect_logout)->with('message', 'logout succeeded');
+        return redirect($this->redirect_logout)->with('message', 'logout succeeded');  
     }
-
-
 }
